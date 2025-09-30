eb2137d809e6aaf5d67e42180a8966ee53c47103
Diren Gunasekera <87416762+diren123@users.noreply.github.com>
2025-09-30 14:28:22 +0100

    Belgium Exercise

--- Python/ex11_starter.py
+++ Python/ex11_starter.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
+from functools import total_ordering
+from traceback import print_tb
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
+# 3a
+length_of_belgium = len(Belgium)
+print("-" * length_of_belgium)
+
+# 3b
+belgium_colon = Belgium.replace(',', ':')
+print(belgium_colon)
+
+#3c
+fields = belgium_colon.split(':')
+
+population_of_Belgium = int(fields[1])
+population_of_Capital_City = int(fields[3])
+total_population = population_of_Belgium + population_of_Capital_City
+
+print(total_population)
